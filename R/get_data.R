a3194075a9ecef628e1b1279ea084a8d3d37d119
Andrei Amatuni <andrei.amatuni@gmail.com>
2017-06-14 16:20:01 -0400

    show full path in debug printing

--- R/get_data.R
+++ R/get_data.R
@@ -2,19 +2,22 @@
 get_all_basiclevel <- function(branch = NULL, commit = NULL, type="feather") {
   switch(type,
          "csv" = get_df_file('all_basiclevel', "all_basiclevel.csv", branch, commit),
-         "feather" = get_df_file('all_basiclevel', "all_basiclevel.feather", branch, commit))
+         "feather" = get_df_file('all_basiclevel', "all_basiclevel.feather", branch, commit)
+         )
 }
 
 get_cdi_spreadsheet <- function(branch = NULL, commit = NULL, type="feather") {
   switch(type,
          "csv" = get_df_file('cdi_spreadsheet', "cdi.csv", branch, commit),
-         "feather" = get_df_file('motor_spreadsheet', "cdi.feather", branch, commit))
+         "feather" = get_df_file('motor_spreadsheet', "cdi.feather", branch, commit)
+         )
 }
 
 get_motor_spreadsheet <- function(branch = NULL, commit = NULL, type="feather") {
   switch(type,
          "csv" = get_df_file('motor_spreadsheet', "motor.csv", branch, commit),
-         "feather" = get_df_file('motor_spreadsheet', "motor.feather", branch, commit))
+         "feather" = get_df_file('motor_spreadsheet', "motor.feather", branch, commit)
+         )
 }
 
 
@@ -23,10 +26,9 @@ get_df_file <- function(repo, filename, branch, commit) {
   file_path <- file.path(blab_data, repo, filename)
   if (endsWith(file_path, ".csv")) {
     result <- read.csv(file_path)
-    print("reading csv")
   } else if (endsWith(file_path, ".feather")) {
     result <- feather::read_feather(file_path)
-    print("reading feather")
   }
+  print(paste("reading file: ", file_path))
   return(result)
 }
