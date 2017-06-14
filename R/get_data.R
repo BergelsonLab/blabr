8242e9ed968df493867970c39a911495a3038401
Andrei Amatuni <andrei.amatuni@gmail.com>
2017-06-14 16:16:27 -0400

    consolidate file picking code

--- R/get_data.R
+++ R/get_data.R
@@ -1,32 +1,32 @@
 
-get_all_basiclevel <- function(branch = NULL, commit = NULL, feather= FALSE) {
-  get_csv_df('all_basiclevel', branch, commit, "all_basiclevel.csv")
+get_all_basiclevel <- function(branch = NULL, commit = NULL, type="feather") {
+  switch(type,
+         "csv" = get_df_file('all_basiclevel', "all_basiclevel.csv", branch, commit),
+         "feather" = get_df_file('all_basiclevel', "all_basiclevel.feather", branch, commit))
 }
 
-get_cdi_spreadsheet <- function(branch = NULL, commit = NULL, feather= FALSE) {
-  get_csv_df('cdi_spreadsheet', branch, commit, "cdi.csv")
+get_cdi_spreadsheet <- function(branch = NULL, commit = NULL, type="feather") {
+  switch(type,
+         "csv" = get_df_file('cdi_spreadsheet', "cdi.csv", branch, commit),
+         "feather" = get_df_file('motor_spreadsheet', "cdi.feather", branch, commit))
 }
 
-get_motor_spreadsheet <- function(branch = NULL, commit = NULL, feather= FALSE) {
-  if (feather) {
-    get_feather_df('motor_spreadsheet', branch, commit, "motor.csv")
-  } else {
-    get_csv_df('motor_spreadsheet', branch, commit, "motor.csv")
+get_motor_spreadsheet <- function(branch = NULL, commit = NULL, type="feather") {
+  switch(type,
+         "csv" = get_df_file('motor_spreadsheet', "motor.csv", branch, commit),
+         "feather" = get_df_file('motor_spreadsheet', "motor.feather", branch, commit))
+}
+
+
+get_df_file <- function(repo, filename, branch, commit) {
+  sync_repo(repo, branch, commit)
+  file_path <- file.path(blab_data, repo, filename)
+  if (endsWith(file_path, ".csv")) {
+    result <- read.csv(file_path)
+    print("reading csv")
+  } else if (endsWith(file_path, ".feather")) {
+    result <- feather::read_feather(file_path)
+    print("reading feather")
   }
-}
-
-get_csv_df <- function(repo = "", branch = NULL, commit = NULL, filename = "") {
-  sync_repo(repo, branch, commit)
-  file_path <- file.path(blab_data, repo, filename)
-  result <- read.csv(file_path)
   return(result)
 }
-
-get_feather_df <- function(repo = "", branch = NULL, commit = NULL, filename = "") {
-  sync_repo(repo, branch, commit)
-  file_path <- file.path(blab_data, repo, filename)
-  result <- feather::read_feather(file_path)
-  return(result)
-}
-
-
