e7f0bdc1fab5d04397caf1a64a89f7adc2786dd5
Andrei Amatuni <andrei.amatuni@gmail.com>
2017-12-08 09:53:01 -0500

    add removeLowData eye tracking function

--- R/eye_tracking.R
+++ R/eye_tracking.R
@@ -44,3 +44,60 @@ binifyFixations <- function(gaze, binSize=20, keepCols=c("Subject","TrialNumber"
 
   return(dataFull)
 }
+
+removeLowData <- function(gazeData=NULL,
+                          subsetData=NULL,
+                          maxBins=NULL,
+                          maxMissing=NULL,
+                          binSize=20,
+                          propt="propt",
+                          timeBin="timeBin",
+                          TrialNumber="TrialNumReal",
+                          SubjectNumber="SubjectNumber"){
+  # this function is for making sure there's at least X amount of data in a trial; there are two potential sources of missing data: 1) off screen 2) elsewhere, not in an interest area
+  #gazeData is the dataset, subsetData is the column name that contains "Y" indicating that's the part in which we are making sure there's enough data,
+  #maxBins is how many bins there could have been in the trial,
+  #minLength is how much data is the minimum to keep the trial, (not arg)
+  #maxMissing= in real time, how many ms of data need to be there
+
+  #binSize is what size of bins the fixations were turned into, this will usually be 20ms,
+  #propt is proportion of target looking,
+
+  #timeBin is the (20 ms) bin the trial that each line is
+
+  gazeData2 <- gazeData[gazeData[,subsetData]=="Y",] #can't use regular subset notation, though could use subset(df, eval(as.name(col)) == "Y")
+  gazeData2 <- as.data.frame(gazeData2)
+
+  #1) offscreen: those timebins don't exist with my version of binifyFixations so how many timebins are there in relation to the maximum given the trial length?
+  number_timebins <- tapply(gazeData2 $timeBin, list(gazeData2 $TrialNumber, gazeData2 $SubjectNumber),length)
+  missing_bins <- maxBins-number_timebins
+
+  #2)elsewhere: let's see how many NAs we have for propt, our proportion of target looking
+
+  gazeData2 $missing<-NA
+  gazeData2 $missing[is.na(gazeData2 $propt)==T]<-1
+  gazeData2 $missing[is.na(gazeData2 $propt)==F]<-0
+
+  elsewhere_bins <- tapply(gazeData2 $missing, list(gazeData2 $TrialNumber, gazeData2 $SubjectNumber), sum)
+
+  missing_bins+elsewhere_bins->lowdata_bins # lowdata_bins is the number of missing bins for every trial for every subject, if what's missing is more than minLength ms, this will be True
+  missing_TF <- ((lowdata_bins)>floor(maxMissing/20))
+  missing_TF <- as.data.frame(missing_TF)
+
+  TrialNumber <- rownames(missing_TF)
+  missing_TF <- cbind(TrialNumber, missing_TF)
+  #okay now we have a df where "False" means it's fine, "NA" means no data for that trial bc the subject ended before the study was over, and "True" means too much missing data
+
+  missingTF <- gather(missing_TF, TrialNumber)
+  names(missingTF) <- c("TrialNumber", "SubjectNumber", "lowdata")
+
+
+  #and merge it back onto our dataframe
+  gazeData <- merge(gazeData, missingTF, by=c("SubjectNumber","TrialNumber"))
+
+  #subset to the part without lowdata, i'm commenting this out for now
+
+  return(gazeData)
+}
+
+
