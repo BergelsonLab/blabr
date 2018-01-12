081d98a67e798052d8878fac602584fb6fc6ea8c
Andrei Amatuni <andrei.amatuni@gmail.com>
2018-01-12 15:14:29 -0500

    more docs

--- R/get_data.R
+++ R/get_data.R
@@ -9,7 +9,11 @@
 #' @export
 #'
 #' @examples
+#' # get the latest
 #' all_bl <- get_all_basiclevel()
+#'
+#' # get version at a specific commit
+#' all_bl <- get_all_basiclevel(commit='833356d604fa2ea020d7984a7f6be612ffea862c')
 get_all_basiclevel <- function(branch = NULL, commit = NULL, type="feather") {
   switch(type,
          "csv" = get_df_file('all_basiclevel', "all_basiclevel.csv", branch, commit),
@@ -54,6 +58,19 @@ get_motor_spreadsheet <- function(branch = NULL, commit = NULL, type="feather") 
          )
 }
 
+
+#' Get the reliability spreadsheets
+#'
+#' @param av either "audio" or "video"
+#' @param month the month as a string, e.g. "06"
+#' @param branch git branch to pull from
+#' @param commit git commit to pull
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' audio_06_rel <- get_reliability("audio", "06")
 get_reliability <- function(av, month, branch = NULL, commit = NULL) {
   fname <- paste0(av, "_", month, ".csv")
   get_df_file('reliability', fname, branch, commit)
--- tests/sync_blabdata.R
+++ tests/sync_blabdata.R
@@ -1,17 +1,18 @@
 library(blabr)
 
 # get the latest
-# all_bl <- get_all_basiclevel()
-#
-# # get the version at a specific commit
-# all_bl <- get_all_basiclevel(commit = '833356d604fa2ea020d7984a7f6be612ffea862c')
-#
-# all_bl <- get_all_basiclevel(branch='no_header')
-#
-# all_bl <- get_all_basiclevel(commit = '5259867974bfa73ba7c1aa83d187c507d50f832c')
-#
-# cdi <- get_cdi_spreadsheet()
-# motor <- get_motor_spreadsheet()
+all_bl <- get_all_basiclevel()
+
+# get the version at a specific commit
+all_bl <- get_all_basiclevel(commit = '833356d604fa2ea020d7984a7f6be612ffea862c')
+
+all_bl <- get_all_basiclevel(branch='no_header')
+
+all_bl <- get_all_basiclevel(commit = '5259867974bfa73ba7c1aa83d187c507d50f832c')
+
+cdi <- get_cdi_spreadsheet()
+motor <- get_motor_spreadsheet()
 
 
 relia <- get_reliability("audio", "06")
+?get_all_basiclevel
