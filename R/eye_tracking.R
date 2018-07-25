edb15a4ffd267c028a63ebb0e4812333beb24aa7
CharlotteMoore927 <charlotte.moore@duke.edu>
2018-07-25 16:07:47 -0400

    new functions: FindLowData, FindFrozenTrials, RemoveFrozenTrials. changed RemoveLowData

--- R/eye_tracking.R
+++ R/eye_tracking.R
@@ -44,8 +44,8 @@ binifyFixations <- function(gaze, binSize=20, keepCols=c("Subject","TrialNumber"
 
   return(dataFull)
 }
-
-RemoveLowData <- function(gazeData,
+#FindLowData----
+FindLowData <- function(gazeData,
                           subsetWin,
                           maxBins = NULL,
                           maxMissing = NULL,
@@ -95,15 +95,72 @@ RemoveLowData <- function(gazeData,
     mutate(missing_TF = lowdata >floor(maxMissing/binSize)) %>%
     select(Trial, SubjectNumber, missing_TF)
 
-  gazeData <- left_join(gazeData, lowdata_bins) %>%
-    filter(missing_TF == F) %>%  # This actually removes the low data bins for you
-    select(-missing_TF)
+  gazeData <- left_join(gazeData, lowdata_bins)
 
-  message("low data bins have now been removed from this dataframe.")
+  message("new column missing_TF has been added. When T, the row has low data.")
   return(gazeData)
 }
 
+#RemoveLowData ----
+RemoveLowData <- function(gazeData,
+                        subsetWin,
+                        maxBins = NULL,
+                        maxMissing = NULL,
+                        binSize = 20,
+                        propt = "propt",
+                        timeBin = "timeBin",
+                        Trial = "Trial",
+                        SubjectNumber = "SubjectNumber") {
+  # this function is for making sure there's at least X amount of data in a trial; there are two potential sources of missing data: 1) off screen 2) elsewhere, not in an interest area
+  #gazeData is the dataset, subsetWin is the column name that contains "Y" indicating that's the part in which we are making sure there's enough data,
+  #maxBins is how many bins there could have been in the trial,
+  #minLength is how much data is the minimum to keep the trial, (not arg)
+  #maxMissing= in real time, how many ms of data need to be there
 
+  #binSize is what size of bins the fixations were turned into, this will usually be 20ms,
+  #propt is proportion of target looking,
+
+  #timeBin is the (20 ms) bin the trial that each line is
+
+
+  gazeData2 <- gazeData %>%
+    filter(gazeData[,subsetWin] == "Y")
+
+  print(dim(gazeData2))
+  #1) offscreen: those timebins don't exist with my version of binifyFixations so how many timebins
+  # are there in relation to the maximum given the trial length?
+
+  number_timebins <- gazeData2 %>%
+    group_by(Trial, SubjectNumber) %>%
+    tally() %>%
+    mutate(bins = n) %>%
+    select(-n)%>%
+    mutate(missing_bins = maxBins - bins)
+
+  #2)elsewhere: let's see how many NAs we have for propt, our proportion of target looking
+
+  elsewhere_bins <- gazeData2 %>%
+    group_by(Trial, SubjectNumber) %>%
+    tally(is.na(propt)) %>%
+    mutate(elsewhere_bins = n) %>%
+    select(-n)
+
+  # This is all the low data from either source
+
+  lowdata_bins <- left_join(number_timebins, elsewhere_bins) %>%
+    mutate(lowdata = missing_bins + elsewhere_bins) %>%
+    mutate(missing_TF = lowdata >floor(maxMissing/binSize)) %>%
+    select(Trial, SubjectNumber, missing_TF)
+
+  gazeData <- left_join(gazeData, lowdata_bins) %>%
+    filter(missing_TF == F) %>%
+    select(-missing_TF)
+
+  message("Low data rows have been removed. To identify them in a new column without removing them, use blabr::FindLowData.")
+  return(gazeData)
+}
+
+#Outlier----
 outlier <- function(cross_item_mean_proptcorrTT, num_sd=3) {
   (cross_item_mean_proptcorrTT >
      (mean(cross_item_mean_proptcorrTT) +
@@ -113,8 +170,39 @@ outlier <- function(cross_item_mean_proptcorrTT, num_sd=3) {
         num_sd*(sd(cross_item_mean_proptcorrTT))))
 }
 
+#expandFixList----
 expandFixList <- function(d, binSize=20){
   timeBin<-(ceiling(d$CURRENT_FIX_START/binSize):ceiling(d$FixEnd/binSize))
   data.frame(timeBin=timeBin,FixationID=d$FixationID)
 }
 
+#FindFrozenTrials----
+FindFrozenTrials <- function(gazeData,
+                             Trial,
+                             SubjectNumber,
+                             gaze) {
+
+ gazeData <-  gazeData %>%
+    group_by(SubjectNumber, Trial) %>%
+    mutate(frozen = ifelse(length(levels(fct_explicit_na(gaze, na_level = "NA"))) == 1, T, F))
+
+ message("Column added identifying trials where gaze stayed in one interest area for whole trial (frozen = T).")
+ return(gazeData)
+}
+
+
+#RemoveFrozenTrials-----
+RemoveFrozenTrials <- function(gazeData,
+                             Trial,
+                             SubjectNumber,
+                             gaze) {
+
+  gazeData <-  gazeData %>%
+    group_by(SubjectNumber, Trial) %>%
+    mutate(frozen = ifelse(length(levels(fct_explicit_na(gaze, na_level = "NA"))) == 1, T, F)) %>%
+    filter(frozen == F) %>%
+    select(-frozen)
+
+  message("frozen trials have been removed. To identify them in a new column without removing them, use blabr::FindFrozenTrials.")
+  return(gazeData)
+}
