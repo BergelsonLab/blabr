d518ae5f6d7c8a7c69eaefc1ac28ad4500d05642
char927 <you@example.com>
2019-07-12 09:23:15 -0400

    theeeeeemes

--- R/theme.R
+++ R/theme.R
@@ -1,6 +1,4 @@
 library(viridis)
-install.packages("extrafontdb")
-install.packages("extrafont")
 library(extrafont)
 font_import(paths = "fonts/", prompt = F)
 fonts() #get a list of fonts
@@ -34,6 +32,7 @@ theme_blab <- function () {
     )
 }
 
+
 plot <- ggplot(df, aes(x = TheQuickBrownFox, y = JumpedOverTheLazyDog, fill = z)) +
   stat_summary(geom = "bar", position = "dodge") +
   scale_fill_viridis(discrete = T) +
@@ -43,7 +42,60 @@ plot <- ggplot(df, aes(x = TheQuickBrownFox, y = JumpedOverTheLazyDog, fill = z)
 plot
 ggsave(plot, filename = "plot.png", bg ="transparent")
 
+theme_spooky <- function() {
+  theme_bw(base_size=32, base_family="serif") %+replace%
+    theme(
+      panel.background = element_rect(fill = "#FFFFFF"),
+      panel.border = element_rect(fill = "transparent", colour = "grey50"),
+      legend.text = element_text(margin = margin(l = 0), colour = "white"),
+      axis.text = element_text(colour = "white"),
+      axis.title = element_text(colour = "white"),
+      plot.background = element_rect(fill = "#0f0838", colour = "#0f0838"),
+      legend.background = element_rect(fill = "#0f0838", colour = "#0f0838"),
+      legend.key = element_blank(),
+      strip.background = element_rect(fill = "#102b52", colour = "grey50"),
+      strip.text = element_text(colour = "white", size = 24, margin = margin(t = 5, 0, b = 5, 0))
+      )
+}
 
+plot.spooky <- ggplot(df, aes(x = TheQuickBrownFox, y = JumpedOverTheLazyDog, fill = z)) +
+  stat_summary(geom = "bar", position = "dodge") +
+  scale_fill_viridis(discrete = T) +
+  facet_grid(~facet) +
+  theme_spooky() +
+  theme(legend.title = element_blank())
+plot.spooky
+ggsave("spooky.pdf")
+
+theme_AMERICA <- function() {
+  theme_bw(base_size=18, base_family="sans") %+replace%
+    theme(
+      panel.background = element_rect(fill = "#FFFFFF"),
+      panel.border = element_rect(fill = "transparent", colour = "grey50"),
+      legend.text = element_text(margin = margin(l = 0), colour = "white"),
+      axis.text = element_text(colour = "white"),
+      axis.title = element_text(colour = "white"),
+      panel.grid.major = element_line(colour = "#BF0A30", size = 8),
+      panel.grid.minor = element_line(colour = "#BF0A30", size = 8),
+      panel.grid.major.x = element_line(colour = "transparent", size = .3),
+      panel.grid.minor.x = element_line(colour = "transparent", size = .3),
+      plot.background = element_rect(fill = "#002868", colour = "#002868"),
+      legend.background = element_rect(fill = "#002868", colour = "#002868"),
+      legend.key = element_blank(),
+      strip.background = element_rect(fill = "white", colour = "grey50"),
+      strip.text = element_text(colour = "#002868", size = 18, margin = margin(t = 5, 0, b = 5, 0))
+    )
+}
+
+plot.merica <- ggplot(df, aes(x = TheQuickBrownFox, y = JumpedOverTheLazyDog, fill = z)) +
+  stat_summary(geom = "bar", position = "dodge") +
+  scale_fill_viridis(discrete = T) +
+  facet_grid(~facet) +
+  theme_AMERICA() +
+  annotate("point", shape = ) +
+  theme(legend.title = element_blank())
+plot.merica
+ggsave("merica.pdf")
 
 printfigs <- "Y" # Set to anything but "Y" to stop the figures from printing
 BW <- "Y" # Set to anything but "Y" for color figures
