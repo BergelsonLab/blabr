0c11fb4595ffe6328443fcface8a06baa508daa3
char927 <you@example.com>
2019-07-12 12:10:17 -0400

    got rid of all the test stuff so now it's just the functions

--- R/theme.R
+++ R/theme.R
@@ -1,16 +1,10 @@
 library(viridis)
 library(extrafont)
 font_import(paths = "fonts/", prompt = F)
-fonts() #get a list of fonts
 ifelse(Sys.info()['sysname'][[1]] == "Windows",
-       extrafont::loadfonts(device = "win"), NULL)
-extrafont::loadfonts(device = "pdf")
+       loadfonts(device = "win"), NULL)
+loadfonts(device = "pdf")
 library(tidyverse)
-
-df <- tibble(TheQuickBrownFox = c("a", "a", "a", "a", "b", "b", "b", "b"),
-            JumpedOverTheLazyDog = c(1,2,3,4,3,4,5,6),
-            z = as_factor(c("This", "is", "the", "Best", "Plot", "you'll", "Ever", "see")),
-            facet = c("little","little","big","big","little","little","big","big"))
 
 theme_blab <- function () {
   theme_bw(base_size=18, base_family="Franklin Gothic Medium") %+replace%
@@ -32,16 +26,6 @@ theme_blab <- function () {
     )
 }
 
-
-plot <- ggplot(df, aes(x = TheQuickBrownFox, y = JumpedOverTheLazyDog, fill = z)) +
-  stat_summary(geom = "bar", position = "dodge") +
-  scale_fill_viridis(discrete = T) +
-  facet_grid(~facet) +
-  theme_blab() +
-  theme(legend.title = element_blank())
-plot
-ggsave(plot, filename = "plot.png", bg ="transparent")
-
 theme_spooky <- function() {
   theme_bw(base_size=32, base_family="serif") %+replace%
     theme(
@@ -57,15 +41,6 @@ theme_spooky <- function() {
       strip.text = element_text(colour = "white", size = 24, margin = margin(t = 5, 0, b = 5, 0))
       )
 }
-
-plot.spooky <- ggplot(df, aes(x = TheQuickBrownFox, y = JumpedOverTheLazyDog, fill = z)) +
-  stat_summary(geom = "bar", position = "dodge") +
-  scale_fill_viridis(discrete = T) +
-  facet_grid(~facet) +
-  theme_spooky() +
-  theme(legend.title = element_blank())
-plot.spooky
-ggsave("spooky.pdf")
 
 theme_AMERICA <- function() {
   theme_bw(base_size=18, base_family="sans") %+replace%
@@ -86,28 +61,3 @@ theme_AMERICA <- function() {
       strip.text = element_text(colour = "#002868", size = 18, margin = margin(t = 5, 0, b = 5, 0))
     )
 }
-
-plot.merica <- ggplot(df, aes(x = TheQuickBrownFox, y = JumpedOverTheLazyDog, fill = z)) +
-  stat_summary(geom = "bar", position = "dodge") +
-  scale_fill_viridis(discrete = T) +
-  facet_grid(~facet) +
-  theme_AMERICA() +
-  annotate("point", shape = ) +
-  theme(legend.title = element_blank())
-plot.merica
-ggsave("merica.pdf")
-
-printfigs <- "Y" # Set to anything but "Y" to stop the figures from printing
-BW <- "Y" # Set to anything but "Y" for color figures
-OL.thresh <- 3 # SD*N threshold for excluding outliers
-corpuscolors <- c("firebrick1", "gold1", "forestgreen", "dodgerblue1")
-corpuslabels <- c("Bergelson", "McDivitt", "VanDam", "Warlaumont")
-agdcolors <- c("maroon", "mediumblue")
-agdcolors.bu <- c("chartreuse3", "blue2")
-agdlabels <- c("Woman  ", "Man  ")
-agecutlabels <- c("<12mo.", ">12mo.")
-#medcolors <- c("gray70", "gray50", "gray20")
-medcolors <- c("#E495A5", "#ABB065", "#39BEB1")
-medlabels <- c("No BA  ", "BA  ", "AD")
-cgdcolors <- c("purple", "yellow")
-cgdlabels <- c("girl", "boy")
