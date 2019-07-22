f27657e9ba975a3bb9899927ee48dd1e4856a4aa
CharlotteMoore927 <charlotte.moore@duke.edu>
2019-07-22 15:28:27 -0400

    finalized cdi_get_words and get_vocab_score

--- R/cdi_wrangling.R
+++ R/cdi_wrangling.R
@@ -1,13 +1,11 @@
 library(tidyverse)
 
-CDI_scores <- read_csv("test_data/test_cdi_data.csv")
-
-rename_cdi_cols <- function(data, cdi_type = "wg") {
+cdi_get_words <- function(data, cdi_type = "wg") {
 
   data <- if (cdi_type == "wg" | cdi_type == "WG") {
-    dplyr::select(data, subject_id, completed, item_34:item_429)
+    dplyr::select(data, subject_id, completed, `baa baa`:some)
   } else {
-    dplyr::select(data, subject_id, completed, item_1:item_680)
+    dplyr::select(data, subject_id, completed, `baa baa`:then)
   }
 
   ifelse(cdi_type == "WG" | cdi_type == "wg", dict <- read_csv("CDI_dict/English_WG_dictionary.csv"),
@@ -25,10 +23,54 @@ rename_cdi_cols <- function(data, cdi_type = "wg") {
   return(data)
 }
 
-attempt <- rename_cdi_cols(CDI_scores, cdi_type = "ws")
+CDI_scores <- read_csv("test_data/test_cdi_ws.csv")
 
-get_vocab_score <- function(data, cdi_type) {
+
+attempt <- cdi_get_words(CDI_scores, cdi_type = "WS")
+data <- attempt
+
+get_vocab_score <- function(data, cdi_type, remove_incomplete = T) {
+
+
+  if (remove_incomplete == T){
+    data <- data %>%
+      filter(completed == T)}
+
+  data <- data %>%
+    gather(key = word, value = value, 3:ncol(data)) %>%
+    mutate(value = as.factor(value)) %>%
+    rename(SubjectNumber = subject_ID) %>%
+    group_by(SubjectNumber, value) %>%
+    tally()
+
+  if (cdi_type == "wg" | cdi_type == "WG") {
+    data <- data %>%
+      mutate(value = fct_recode(value,
+                                "understands"="understands",
+                                "produces" = "produces",
+                                "neither" = "NA"))
+  } else {
+    data <- data %>%
+      mutate(value = fct_recode(value,
+                                "understands"="understands",
+                                "neither" = "NA"))
+  }
+
+  data <- data %>%
+    spread(value, n, fill=0)
+
+  if (cdi_type == "wg" | cdi_type == "WG") {
+    data <- data %>%
+      mutate(CDIcomp = understands+produces,
+             CDIprod = produces) %>%
+      select(SubjectNumber, CDIcomp, CDIprod)
+  } else {
+    data <- data %>%
+      select(SubjectNumber, produces) %>%
+      rename(CDIprod = produces)
+  }
+  return(data)
 
 }
 
-
+attempt2 <- get_vocab_score(attempt, cdi_type = "WS", remove_incomplete = F)
