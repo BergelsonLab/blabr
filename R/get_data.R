183d63c6d8fda08b572df939d0cc3ba9c7b01a02
Zhenya <e.kalenkovich@gmail.com>
2022-02-09 10:51:55 -0500

    switch from commits/branches to version tags

--- R/get_data.R
+++ R/get_data.R
@@ -1,60 +1,56 @@
 
 #' Get the all_basiclevel data
 #'
-#' @param branch git branch to pull from
-#' @param commit git commit to pull
+#' @param version version tag to checkout
 #' @param type "feather" or "csv". defaults to "feather"
 #'
-#' @return a tibble containing the all_basicalevel data
+#' @return a dataframe containing the all_basicalevel data
 #' @export
 #'
 #' @examples
-#' # get the latest
-#' all_bl <- get_all_basiclevel()
 #'
-#' # get version at a specific commit
-#' all_bl <- get_all_basiclevel(commit='833356d604fa2ea020d7984a7f6be612ffea862c')
-get_all_basiclevel <- function(branch = NULL, commit = NULL, type="feather") {
+#' # get version with a specific version tag
+#' all_bl <- get_all_basiclevel(version='0.1.0')
+get_all_basiclevel <- function(version = NULL, type="feather") {
   switch(type,
-         "csv" = get_df_file('all_basiclevel', "all_basiclevel.csv", branch, commit),
-         "feather" = get_df_file('all_basiclevel', "all_basiclevel.feather", branch, commit)
+         "csv" = get_df_file('all_basiclevel', "all_basiclevel.csv", version = version),
+         "feather" = get_df_file('all_basiclevel', "all_basiclevel.feather", version = version)
          )
 }
 
 
 #' Get the CDI spreadsheet for SEEDLingS babies
 #'
-#' @param branch git branch to pull from
-#' @param commit git commit to pull
+#' @param version version tag to checkout
 #' @param type "feather" or "csv". defaults to "feather"
 #'
 #' @return a tibble containing the SEEDLingS CDI spreadsheet
 #' @export
 #'
 #' @examples
-#' cdi <- get_cdi_spreadsheet()
-get_cdi_spreadsheet <- function(branch = NULL, commit = NULL, type="feather") {
+#' cdi <- get_cdi_spreadsheet(version='0.0.7')
+get_cdi_spreadsheet <- function(version = NULL, type="feather") {
   switch(type,
-         "csv" = get_df_file('cdi_spreadsheet', "cdi.csv", branch, commit),
-         "feather" = get_df_file('cdi_spreadsheet', "cdi.feather", branch, commit)
+         "csv" = get_df_file('cdi_spreadsheet', "cdi.csv", version = version),
+         "feather" = get_df_file('cdi_spreadsheet', "cdi.feather", version = version)
          )
 }
 
 
 #' The the motor questionaire spreadsheet for the SEEDLingS babies
 #'
-#' @param branch git branch to pull from
-#' @param commit git commit to pull
+#' @param version version tag to checkout
 #' @param type "feather" or "csv". defaults to "feather"
 #'
 #' @return a tibble contaiing the SEEDLingS Motor Questionaire spreadsheet
 #' @export
 #'
 #' @examples
-get_motor_spreadsheet <- function(branch = NULL, commit = NULL, type="feather") {
+#' motor <- get_motor_spreadsheet(version = '0.1.0')
+get_motor_spreadsheet <- function(version = NULL, type="feather") {
   switch(type,
-         "csv" = get_df_file('motor_spreadsheet', "motor.csv", branch, commit),
-         "feather" = get_df_file('motor_spreadsheet', "motor.feather", branch, commit)
+         "csv" = get_df_file('motor_spreadsheet', "motor.csv", version = version),
+         "feather" = get_df_file('motor_spreadsheet', "motor.feather", version = version)
          )
 }
 
@@ -63,21 +59,86 @@ get_motor_spreadsheet <- function(branch = NULL, commit = NULL, type="feather") 
 #'
 #' @param av either "audio" or "video"
 #' @param month the month as a string, e.g. "06"
-#' @param branch git branch to pull from
-#' @param commit git commit to pull
+#' @param version version tag to checkout
 #'
-#' @return
+#' @return dataframe with data for the requested modality and month
 #' @export
 #'
 #' @examples
 #' audio_06_rel <- get_reliability("audio", "06")
-get_reliability <- function(av, month, branch = NULL, commit = NULL) {
+get_reliability <- function(av, month, version = NULL) {
   fname <- paste0(av, "_", month, ".csv")
-  get_df_file('reliability', fname, branch, commit)
+  get_df_file('reliability', fname, version)
 }
 
-get_df_file <- function(repo, filename, branch, commit) {
-  sync_repo(repo, branch, commit)
+
+#' Find latest version available for downloading?
+#'
+#' @inheritParams get_latest_tag
+#' @inheritParams
+#'
+#' @return the version string
+#' @export
+#'
+#' @examples
+#' get_latest_version('all_basiclvel')
+get_latest_version <- function(repo, tags_already_updated = FALSE) {
+  get_latest_tag(repo, tags_already_updated = tags_already_updated)
+}
+
+
+#' Check whether there is a newer version than one requested
+#'
+#' @inheritParams get_latest_tag
+#' @param requested_version - the version user requested
+#'
+#' @examples
+#' check_for_updates('all_basiclevel', '0.0.2')
+check_for_updates <- function(repo, requested_version,
+                              tags_already_updated = FALSE) {
+  latest_version <- get_latest_version(
+    repo, tags_already_updated = tags_already_updated)
+  if (latest_version != requested_version) {
+    message(glue::glue(
+      "You've requested version {requested_version} of {repo}.\n",
+      "The latest available version is {latest_version}.\n",
+      "Consider updating."))
+  }
+}
+
+
+#' Downloads and then loads a csv/feather file from a specified version of a
+#' dataset
+#'
+#' @inheritParams get_all_basiclevel
+#' @inheritParams get_latest_tag
+#' @param filename name of a csv/feather file
+#'
+#' @return tibble for feather files, data.frame for csv files
+#'
+#' @examples
+#' get_df_file('all_basiclevel', 'all_basiclevel.csv', version = '0.1.0')
+get_df_file <- function(repo, filename, version = NULL) {
+  # Get the up-to-date set of tags
+  update_tags(repo)
+
+  # Find out the latest version if none was specified
+  if (is.null(version)) {
+    requested_version <- get_latest_version(repo, tags_already_updated = TRUE)
+    warning(glue::glue(
+      'Getting a dataset without specifying a version is highly discouraged.\n',
+      'Add ", version = {requested_version}" to the `get_*` function call.'))
+  } else {
+    requested_version <- version
+    # Check if there is a newer version
+    check_for_updates(repo = repo, requested_version = requested_version,
+                      tags_already_updated = TRUE)
+  }
+
+  # Download the file
+  checkout_tag(repo, tag = requested_version)
+
+  # Load the file
   file_path <- file.path(blab_data, repo, filename)
   if (endsWith(file_path, ".csv")) {
     result <- read.csv(file_path)
@@ -97,7 +158,7 @@ get_df_file <- function(repo, filename, branch, commit) {
 #'
 #' @param dataset - name of the dataset
 #'
-#' @return
+#' @return list with two keys: commit and date
 #' @export
 #'
 #' @examples
--- tests/sync_blabdata.R
+++ tests/sync_blabdata.R
@@ -1,18 +1,9 @@
 library(blabr)
 
 # get the latest
-all_bl <- get_all_basiclevel()
-
-# get the version at a specific commit
-all_bl <- get_all_basiclevel(commit = '833356d604fa2ea020d7984a7f6be612ffea862c')
-
-all_bl <- get_all_basiclevel(branch='no_header')
-
-all_bl <- get_all_basiclevel(commit = '5259867974bfa73ba7c1aa83d187c507d50f832c')
+all_bl <- get_all_basiclevel(version = '0.1.0')
 
 cdi <- get_cdi_spreadsheet()
 motor <- get_motor_spreadsheet()
-
-
 relia <- get_reliability("audio", "06")
-?get_all_basiclevel
+
