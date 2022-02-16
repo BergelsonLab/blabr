2407e5745d2837202fc919a0038a3856c1fba822
Zhenya <e.kalenkovich@gmail.com>
2022-02-16 11:31:13 -0500

    make feather and csv produce similar outputs

--- R/get_data.R
+++ R/get_data.R
@@ -11,11 +11,36 @@
 #'
 #' # get version with a specific version tag
 #' all_bl <- get_all_basiclevel(version='0.1.0')
-get_all_basiclevel <- function(version = NULL, type="feather") {
-  switch(type,
-         "csv" = get_df_file('all_basiclevel', "all_basiclevel.csv", version = version),
-         "feather" = get_df_file('all_basiclevel', "all_basiclevel.feather", version = version)
-         )
+get_all_basiclevel <- function(version = NULL, type = "feather") {
+  stopifnot(type %in% c('feather', 'csv'))
+  filename <- paste0("all_basiclevel.", type)
+
+  col_types <- switch(type,
+    "csv" = readr::cols(
+      .default = readr::col_factor(),
+      # With include_na = FALSE, NA values get coded as belonging to a NA level,
+      # and with include_na = TRUE - to a "NA" level. The latter was easier to
+      # remove in the mutate below, so that we have NA values, not a NA/"NA"
+      # level.
+      tier = readr::col_factor(include_na = TRUE),
+      ordinal = readr::col_integer(),
+      onset = readr::col_integer(),
+      offset = readr::col_integer(),
+      annotid = readr::col_character(),
+      pho = readr::col_character()),
+    "feather" = NULL)
+
+  all_bl <- get_df_file(repo = 'all_basiclevel', filename = filename,
+                        version = version, col_types = col_types)
+
+  if (type == "csv") {
+    all_bl <- all_bl %>%
+      # Drop the "NA" level, converting the corresponding values to NA. Done for
+      # the sake of consistency with reading from the feather file.
+      mutate(tier = recode_factor(all_bl$tier, "NA" = NA_character_))
+  }
+
+  return(all_bl)
 }
 
 
@@ -30,9 +55,17 @@ get_all_basiclevel <- function(version = NULL, type="feather") {
 #' @examples
 #' cdi <- get_cdi_spreadsheet(version='0.0.7')
 get_cdi_spreadsheet <- function(version = NULL, type="feather") {
+  col_types <- readr::cols(
+    .default = readr::col_character(),
+    month = readr::col_double(),
+    CDIcomp = readr::col_double(),
+    CDIprod = readr::col_double()
+  )
   switch(type,
-         "csv" = get_df_file('cdi_spreadsheet', "cdi.csv", version = version),
-         "feather" = get_df_file('cdi_spreadsheet', "cdi.feather", version = version)
+         "csv" = get_df_file('cdi_spreadsheet', "cdi.csv", version = version,
+                             col_types = col_types),
+         "feather" = get_df_file('cdi_spreadsheet', "cdi.feather",
+                                 version = version)
          )
 }
 
@@ -48,9 +81,26 @@ get_cdi_spreadsheet <- function(version = NULL, type="feather") {
 #' @examples
 #' motor <- get_motor_spreadsheet(version = '0.1.0')
 get_motor_spreadsheet <- function(version = NULL, type="feather") {
+  col_types <- readr::cols(
+    .default = readr::col_character(),
+    weight = readr::col_double(),
+    Q8_FirstClick = readr::col_double(),
+    Q8_LastClick = readr::col_double(),
+    Q8_PageSubmit = readr::col_double(),
+    Q8_ClickCount = readr::col_double(),
+    Q26_FirstClick = readr::col_double(),
+    Q26_LastClick = readr::col_double(),
+    Q26_PageSubmit = readr::col_double(),
+    Q26_ClickCount = readr::col_double(),
+    walking_month = readr::col_double(),
+    walking_day = readr::col_double(),
+    walking_year = readr::col_double()
+  )
   switch(type,
-         "csv" = get_df_file('motor_spreadsheet', "motor.csv", version = version),
-         "feather" = get_df_file('motor_spreadsheet', "motor.feather", version = version)
+         "csv" = get_df_file('motor_spreadsheet', "motor.csv",
+                             version = version, col_types = col_types),
+         "feather" = get_df_file('motor_spreadsheet', "motor.feather",
+                                 version = version)
          )
 }
 
@@ -113,12 +163,13 @@ check_for_updates <- function(repo, requested_version,
 #' @inheritParams get_all_basiclevel
 #' @inheritParams get_latest_tag
 #' @param filename name of a csv/feather file
+#' @param col_types Passed to `readr::read_csv` when filename ends with ".csv".
 #'
 #' @return tibble for feather files, data.frame for csv files
 #'
 #' @examples
 #' get_df_file('all_basiclevel', 'all_basiclevel.csv', version = '0.1.0')
-get_df_file <- function(repo, filename, version = NULL) {
+get_df_file <- function(repo, filename, version = NULL, col_types = NULL) {
   # Get the up-to-date set of tags
   update_tags(repo)
 
@@ -141,11 +192,11 @@ get_df_file <- function(repo, filename, version = NULL) {
   # Load the file
   file_path <- file.path(blab_data, repo, filename)
   if (endsWith(file_path, ".csv")) {
-    result <- read.csv(file_path)
+    result <- readr::read_csv(file_path, col_types = col_types)
   } else if (endsWith(file_path, ".feather")) {
     result <- arrow::read_feather(file_path)
   }
-  print(paste("reading file: ", file_path))
+  message("reading file: ", file_path)
   return(result)
 }
 
