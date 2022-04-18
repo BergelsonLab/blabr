902e8bcaafbcaf6249fa8e6381739ac68bed1d2e
Zhenya <e.kalenkovich@gmail.com>
2022-04-18 13:26:08 -0400

    stop updating the dict
    It has already been done, no reason to update it every time.
    Moreover, a type in "new_entries....csv" led to meaningless duplicates (thus, the hashes had to be changed).

--- R/global_basic_level.r
+++ R/global_basic_level.r
@@ -35,26 +35,13 @@ add_global_basic_level <- function(all_basiclevel_na,
     )
   )
 
-  new_global_bls <- readr::read_csv(
-    file.path(mapping_dir, "new_entries_for_all_bl_disamb_feb2021.csv"),
-    col_types = cols(
-      object = col_character(),
-      basic_level = col_character(),
-      global_bl = col_character()
-    ))
-
-  dict_full <- full_join(dict, new_global_bls) %>%
-    dplyr::select(-basic_level) # %>%
-  # write_csv("data/global_bl_dictionary.csv")
-
-
   all_bl_NA3 <- all_bl_NA2 %>%
     dplyr::mutate(disambiguate = dplyr::case_when(
       object == "balls" & basic_level == "ball" & is.na(disambiguate) ~ "toy",
       object == "Momo" & basic_level == "Momo" & is.na(disambiguate) ~ "dog",
       object == "glasses" & basic_level == "glasses" & is.na(disambiguate) ~ "eye",
       TRUE ~ disambiguate)) %>%
-    dplyr::left_join(dict_full, by = c("object", "disambiguate"))
+    dplyr::left_join(dict, by = c("object", "disambiguate"))
 
   duplicate_lines <- all_bl_NA3 %>%
     dplyr::count(annotid) %>%
@@ -89,5 +76,5 @@ add_global_basic_level <- function(all_basiclevel_na,
     dplyr::distinct()
 
   # write_feather(all_bl_NA3, "data/all_bl_global.feather")
-  return(list(all_bl_NA3, dict_full, to_fix))
+  return(list(all_bl_NA3, dict, to_fix))
 }
--- tests/testthat/test-global_basic_level.r
+++ tests/testthat/test-global_basic_level.r
@@ -7,7 +7,7 @@ test_that("add_global_basic_level works as expected", {
   output <- add_global_basic_level(all_basiclevel_na, mappings_dir)
 
   with_global_basic_level <- output[[1]]
-  dict_updated <- output[[2]]
+  dict <- output[[2]]
   to_fix <- output[[3]]
 
   hashes <- with_global_basic_level %>%
@@ -15,25 +15,26 @@ test_that("add_global_basic_level works as expected", {
            across(where(is.factor), as.character)) %>%
     summarise(across(everything(), digest)) %>%
     as.list
-  expected_hashes <- list(
-      ordinal = "2e582ed0c51fae1ada3c9662b141ad08",
-      onset = "0625f731aa050546ef0289b2ae56b389",
-      offset = "d092130aa7854906e84df8fd4d7a0b0f",
-      object = "c310477329cf424ac96a9a0caa66386a",
-      utterance_type = "5fbf641a06d847bb73f4768cf7771a82",
-      object_present = "128e9d7a686e8c274a40a145cb84293d",
-      speaker = "f79b930d9e2d2402a345248158020449",
-      basic_level = "c2922cb5e5b1eed99a9c5f7a48c07cb4",
-      annotid = "beae0ba0a1ab7911f2e7ec694d2afb69",
-      id = "0d5e73858b691d95eb67c8cf23d6ca9e",
-      subj = "576c8ad0be33dcd6623ed05659f182a5",
-      month = "c62cad2e5d97a87c178b1f1d6f926010",
-      SubjectNumber = "bebe952597ccc7179095b39fa833d21c",
-      audio_video = "573803020a672ab792dc19002584c5e8",
-      tier = "9fd48b913f0a73e2f7eae40223a5881f",
-      pho = "c1f1fdde36641ed0bc8b832f4027ba3f",
-      disambiguate = "7d98b93357c916fdc0967851c533e29b",
-      global_bl = "0d2731d2b07593222025ed9ecb0523d1"
+  expected_hashes <-
+    list(
+      ordinal = "2a1a86e041ca65962428b8b49d938f7a",
+      onset = "6c03cd25ef0780feb55d15fc44472dfd",
+      offset = "33b0bd6f6d7181286ae3abdcceb50d06",
+      object = "28f0b9cf94fc753b4525c5e5def55e57",
+      utterance_type = "059ff29157bba28b23a48f7dbb5d3455",
+      object_present = "c32de968fb2578ed4d866286dbb35cca",
+      speaker = "60bc11e0a31badfd15648dd12656df2f",
+      basic_level = "fa3ea6bf10b33ee64046e58d0cd8cd68",
+      annotid = "90aa9d4ef632694c317ce2aa8c3b8aa3",
+      id = "5dc218556e7c202cb8f24af0929490b6",
+      subj = "b594d4cb8774ab705b5109d38e2d5e72",
+      month = "4f0f1775b7ae9e72d095ffd18ca91e75",
+      SubjectNumber = "5667aadce4d103da567eb7d5f5c7f6c0",
+      audio_video = "31e2bbaf624b62c49b842429763a2f23",
+      tier = "4800e1b480ebaa4810436aa3bfe7a839",
+      pho = "1e99888bc40cf6d45c7a0314ff3f8b93",
+      disambiguate = "41dbe1859f00df4d03e2e96f3ac28312",
+      global_bl = "4cb188f0a6508d9fac4deacb3cf5cac7"
     )
   expect_equal(hashes, expected_hashes)
 
@@ -42,10 +43,9 @@ test_that("add_global_basic_level works as expected", {
            across(where(is.factor), as.character)) %>%
     summarise(across(everything(), digest)) %>%
     as.list
-  expected_hashes_dict_updated <- list(
-    object = "9079da00132de3805251f085dcd6abee",
-    disambiguate = "8312c7f18c6308052a26cea4ddba4f57",
-    global_bl = "347b078a4b542f05ecc08b61a46a3ae0")
+  expected_hashes_dict_updated <- list(object = "14bfa11490b253c5a6dc729724175b51",
+                                       disambiguate = "34da6e26409c102ee1ea9e3877e3003c",
+                                       global_bl = "b5e51f8bb04fd022edc3f1f1fb2ed404")
   expect_equal(hashes_dict_updated, expected_hashes_dict_updated)
 
   hashes_to_fix <- to_fix %>%
@@ -54,9 +54,9 @@ test_that("add_global_basic_level works as expected", {
     summarise(across(everything(), digest)) %>%
     as.list
   expected_hashes_to_fix <-
-    list(object = "753fef2edea1f1d91f3d9580dba19d4c",
-         basic_level = "d27782eece28fe34c8e816a29b2bc750",
-         global_bl = "b0c9c0e8cd9e2383df6734dc0d1aa029")
+    list(object = "750b2a3394a8bd154cec0bc043e44e61",
+         basic_level = "f98e49bca949e21577a533b5a712aac7",
+         global_bl = "acdb3a7ad6e9e51c4a8b87de2a9ef76a")
   expect_equal(hashes_to_fix, expected_hashes_to_fix)
 
 })
