94c3c8b341fee0e443a1b8f23094774f72ee2546
Zhenya <e.kalenkovich@gmail.com>
2022-04-19 14:40:59 -0400

    simplify make_global_basic_level code

--- R/global_basic_level.r
+++ R/global_basic_level.r
@@ -1,80 +1,120 @@
 # The "basic_level" column in the dataset collapses version of the same word:
 # "doggie", "dogs", and "dog" get mapped to "dog". This is done at the recording
-# level. Here, we create a *global* basic level version of that done at the
-# corpus level.
+# level. Here, we map objects to a *global* basic level version at the corpus
+# level.
+#
+# There are three sources used for this mapping:
+# 1. object_dict that maps `object` -> `global_bl`. Additionally, it maps
+#    (`object`, `disambiguate`) -> `global_bl` when the object is ambiguous.
+#    When it is not, the `disambiguate` column is NA.
+# 2. annotid_disambiguation that maps `annotid` -> `disambiguate`. To avoid the
+#    situation where `object` was updated, `object` is mathed too.
+# 3. Some of the "-> `disambiguate`" mapping can be done on the basis of
+#    `object`-`basic_level` combination, this is done directly in the code.
+#
+# The mapping is done in two steps:
+# - The `disambiguate` column is filled using sources 2 and 3.
+# - object_dict is used to map both ambigous and unambigous (`disambiguate` is
+#   NA)
 
 
-add_global_basic_level <- function(all_basiclevel_na,
-                                   mappings_dir) {
+
+read_object_dict <- function(object_dict_path) {
+  # Loads and checks the dictionary that maps objects (possibly disambiguated)
+  # to their global basic level.
+
+  object_dict <- object_dict_path %>%
+    readr::read_csv(
+      col_types = readr::cols(
+        object = readr::col_character(),
+        disambiguate = readr::col_character(),
+        global_bl = readr::col_character()
+      ))
+
+  # Any object with multiple rows must have no NAs in the `disambiguate` column.
+  # One NA would act as "else" condition, multiple NAs would result in duplicate
+  # rows matched to the same token (annotid).
+  object_dict %>%
+    dplyr::group_by(object) %>%
+    dplyr::mutate(count = dplyr::n(),
+                  count_NA = sum(is.na(disambiguate))) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(count > 1 & count_NA > 0) %>%
+    nrow %>%
+    assertthat::are_equal(0)
+
+  # There should be no `object`-`disambiguate` duplicates
+  object_dict %>%
+    count(object, disambiguate, name = 'count') %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(count > 1) %>%
+    nrow %>%
+    assertthat::are_equal(0)
+
+  object_dict
+}
+
+
+read_annotid_disambiguation <- function(annotid_disambiguation_path) {
+  # Loads and checks the dictionary mapping annotids to their disambiguation
+  # variant.
+  annotid_disambiguation <- annotid_disambiguation_path %>%
+    readr::read_csv(
+      col_select = c(annotid, object, disambiguate),
+      col_types = readr::cols(
+        object = readr::col_character(),
+        annotid = readr::col_character(),
+        disambiguate = readr::col_character()
+      ))
+
+  annotid_disambiguation %>%
+    count(annotid, name = 'count') %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(count > 1) %>%
+    nrow %>%
+    assertthat::are_equal(0)
+
+  annotid_disambiguation
+}
+
+
+make_global_basic_level <- function(all_basiclevel_na,
+                                    object_dict,
+                                    annotid_disambiguation) {
   # Check that there are rows with NA in the basic_level column
   assertthat::assert_that(sum(is.na(all_basiclevel_na$basic_level)) > 0)
 
+  # Check that the inputs have expected columns (all_basiclevel_na is checked
+  # implicitly via dplyr::select later)
+  assertthat::assert_that(identical(
+    sort(colnames(object_dict)),
+    c("disambiguate", "global_bl", "object")
+  ))
+  assertthat::assert_that(identical(
+    sort(colnames(annotid_disambiguation)),
+    c("annotid", "disambiguate", "object")
+  ))
 
-  dict <- readr::read_csv(
-    file.path(mappings_dir, "global_bl_dictionary.csv"),
-    col_types = cols(
-      object = col_character(),
-      disambiguate = col_character(),
-      global_bl = col_character()
-    ))
-  disamb_rows <- readr::read_csv(
-    file.path(mappings_dir, "disambiguated_rows.csv"),
-    col_select = c(annotid, object, disambiguate),
-    col_types = cols(
-      object = col_character(),
-      annotid = col_character(),
-      disambiguate = col_character()
-    ))
-
-  all_bl_NA2 <- dplyr::left_join(
-    all_basiclevel_na,
-    disamb_rows,
-    by = c(
-      "object",
-      "annotid"
-    )
-  )
-
-  all_bl_NA3 <- all_bl_NA2 %>%
+  # Disambiguate first based on annotid and then based on
+  with_global_bl <- all_basiclevel_na %>%
+    dplyr::select(annotid, object, basic_level) %>%
+    dplyr::left_join(annotid_disambiguation, by = c("annotid", "object")) %>%
     dplyr::mutate(disambiguate = dplyr::case_when(
       object == "balls" & basic_level == "ball" & is.na(disambiguate) ~ "toy",
       object == "Momo" & basic_level == "Momo" & is.na(disambiguate) ~ "dog",
       object == "glasses" & basic_level == "glasses" & is.na(disambiguate) ~ "eye",
-      TRUE ~ disambiguate)) %>%
-    dplyr::left_join(dict, by = c("object", "disambiguate"))
+      TRUE ~ disambiguate))
 
-  duplicate_lines <- all_bl_NA3 %>%
-    dplyr::count(annotid) %>%
-    dplyr::filter(n>1)
+  # Match to the global basic value
+  with_global_bl <- with_global_bl %>%
+    dplyr::left_join(object_dict, by = c("object", "disambiguate"))
+  # Check that we didn't lose any tokens, didn't create any duplicates,
+  assertthat::are_equal(all_basiclevel_na$annotid, with_global_bl$annotid)
 
-  to_fix <- all_bl_NA3 %>%
-    dplyr::filter(annotid %in% duplicate_lines$annotid) %>%
-    dplyr::left_join(
-      all_bl_NA3,
-      by = c(
-        "ordinal",
-        "onset",
-        "offset",
-        "object",
-        "utterance_type",
-        "object_present",
-        "speaker",
-        "basic_level",
-        "annotid",
-        "id",
-        "subj",
-        "month",
-        "SubjectNumber",
-        "audio_video",
-        "tier",
-        "pho",
-        "disambiguate",
-        "global_bl"
-      )
-    ) %>%
-    dplyr::select(object, basic_level, global_bl) %>%
-    dplyr::distinct()
+  # and found global_basic_level for all tokens
+  # without_global_bl_count <- sum(is.na(with_global_bl$global_bl))
+  # warning(glue::glue('There were {without_global_bl_count} tokens for which we
+  #                     couldn\'t assign global basic level.'))
 
-  # write_feather(all_bl_NA3, "data/all_bl_global.feather")
-  return(list(all_bl_NA3, dict, to_fix))
+  return(list(with_global_bl = with_global_bl))
 }
--- tests/testthat/test-global_basic_level.r
+++ tests/testthat/test-global_basic_level.r
@@ -4,11 +4,14 @@ test_that("add_global_basic_level works as expected", {
   all_basiclevel_na <- get_all_basiclevel(version = '0.1.3',
                                           drop_basic_level_na = FALSE)
   mappings_dir <- file.path(blab_data, 'global_basic_level', 'data')
-  output <- add_global_basic_level(all_basiclevel_na, mappings_dir)
+  object_dict <- read_object_dict(
+    file.path(mappings_dir, "global_bl_dictionary.csv"))
+  annotid_disambiguation <- read_annotid_disambiguation(
+    file.path(mappings_dir, "disambiguated_rows.csv"))
 
-  with_global_basic_level <- output[[1]]
-  dict <- output[[2]]
-  to_fix <- output[[3]]
+  with_global_basic_level <- make_global_basic_level(
+    all_basiclevel_na, object_dict,
+    annotid_disambiguation)$with_global_bl
 
   hashes <- with_global_basic_level %>%
     mutate(across(where(is.numeric), as.numeric),
@@ -17,46 +20,11 @@ test_that("add_global_basic_level works as expected", {
     as.list
   expected_hashes <-
     list(
-      ordinal = "2a1a86e041ca65962428b8b49d938f7a",
-      onset = "6c03cd25ef0780feb55d15fc44472dfd",
-      offset = "33b0bd6f6d7181286ae3abdcceb50d06",
-      object = "28f0b9cf94fc753b4525c5e5def55e57",
-      utterance_type = "059ff29157bba28b23a48f7dbb5d3455",
-      object_present = "c32de968fb2578ed4d866286dbb35cca",
-      speaker = "60bc11e0a31badfd15648dd12656df2f",
-      basic_level = "fa3ea6bf10b33ee64046e58d0cd8cd68",
-      annotid = "90aa9d4ef632694c317ce2aa8c3b8aa3",
-      id = "5dc218556e7c202cb8f24af0929490b6",
-      subj = "b594d4cb8774ab705b5109d38e2d5e72",
-      month = "4f0f1775b7ae9e72d095ffd18ca91e75",
-      SubjectNumber = "5667aadce4d103da567eb7d5f5c7f6c0",
-      audio_video = "31e2bbaf624b62c49b842429763a2f23",
-      tier = "4800e1b480ebaa4810436aa3bfe7a839",
-      pho = "1e99888bc40cf6d45c7a0314ff3f8b93",
-      disambiguate = "41dbe1859f00df4d03e2e96f3ac28312",
-      global_bl = "4cb188f0a6508d9fac4deacb3cf5cac7"
+      annotid = "207e1b1c9755effaa844e06d6d726476",
+      object = "dccfe65b3fe6407b3dbf5906308eed82",
+      basic_level = "08d332c4943e65a312b7b420ad4c29bf",
+      disambiguate = "ac9b8c415095981cb048e676d4e14b4f",
+      global_bl = "8b41b0f9a64f52649167dff170d23390"
     )
   expect_equal(hashes, expected_hashes)
-
-  hashes_dict_updated <- dict_updated %>%
-    mutate(across(where(is.numeric), as.numeric),
-           across(where(is.factor), as.character)) %>%
-    summarise(across(everything(), digest)) %>%
-    as.list
-  expected_hashes_dict_updated <- list(object = "14bfa11490b253c5a6dc729724175b51",
-                                       disambiguate = "34da6e26409c102ee1ea9e3877e3003c",
-                                       global_bl = "b5e51f8bb04fd022edc3f1f1fb2ed404")
-  expect_equal(hashes_dict_updated, expected_hashes_dict_updated)
-
-  hashes_to_fix <- to_fix %>%
-    mutate(across(where(is.numeric), as.numeric),
-           across(where(is.factor), as.character)) %>%
-    summarise(across(everything(), digest)) %>%
-    as.list
-  expected_hashes_to_fix <-
-    list(object = "750b2a3394a8bd154cec0bc043e44e61",
-         basic_level = "f98e49bca949e21577a533b5a712aac7",
-         global_bl = "acdb3a7ad6e9e51c4a8b87de2a9ef76a")
-  expect_equal(hashes_to_fix, expected_hashes_to_fix)
-
 })
