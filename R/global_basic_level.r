b594feb33af8536d8a1ab3ba706023a44112b6d5
Zhenya <e.kalenkovich@gmail.com>
2022-04-26 10:26:27 -0400

    make new global basic level

--- R/global_basic_level.r
+++ R/global_basic_level.r
@@ -17,19 +17,19 @@
 # - object_dict is used to map both ambigous and unambigous (`disambiguate` is
 #   NA)
 
+# Placeholder for values that require manual editing
+FIXME <- '***FIX ME***'
 
 
-read_object_dict <- function(object_dict_path) {
-  # Loads and checks the dictionary that maps objects (possibly disambiguated)
-  # to their global basic level.
-
-  object_dict <- object_dict_path %>%
-    readr::read_csv(
-      col_types = readr::cols(
-        object = readr::col_character(),
-        disambiguate = readr::col_character(),
-        global_bl = readr::col_character()
-      ))
+#' Checks object_dict for correct columns and no duplicates
+#'
+#' In case it was not read with `read_object_dict`
+#' @noRd
+check_object_dict <- function(object_dict) {
+  assertthat::assert_that(identical(
+    sort(colnames(object_dict)),
+    c("disambiguate", "global_bl", "object")
+  ))
 
   # Any object with multiple rows must have no NAs in the `disambiguate` column.
   # One NA would act as "else" condition, multiple NAs would result in duplicate
@@ -50,53 +50,225 @@ read_object_dict <- function(object_dict_path) {
     dplyr::filter(count > 1) %>%
     nrow %>%
     assertthat::are_equal(0)
-
-  object_dict
 }
 
 
-read_annotid_disambiguation <- function(annotid_disambiguation_path) {
-  # Loads and checks the dictionary mapping annotids to their disambiguation
-  # variant.
-  annotid_disambiguation <- annotid_disambiguation_path %>%
-    readr::read_csv(
-      col_select = c(annotid, object, disambiguate),
-      col_types = readr::cols(
-        object = readr::col_character(),
-        annotid = readr::col_character(),
-        disambiguate = readr::col_character()
-      ))
-
-  annotid_disambiguation %>%
-    count(annotid, name = 'count') %>%
-    dplyr::ungroup() %>%
-    dplyr::filter(count > 1) %>%
-    nrow %>%
-    assertthat::are_equal(0)
-
-  annotid_disambiguation
-}
-
-
-make_global_basic_level <- function(all_basiclevel_na,
-                                    object_dict,
-                                    annotid_disambiguation) {
-  # Check that there are rows with NA in the basic_level column
-  assertthat::assert_that(sum(is.na(all_basiclevel_na$basic_level)) > 0)
-
-  # Check that the inputs have expected columns (all_basiclevel_na is checked
-  # implicitly via dplyr::select later)
-  assertthat::assert_that(identical(
-    sort(colnames(object_dict)),
-    c("disambiguate", "global_bl", "object")
-  ))
+#' Check annotid_disambiguation for correct columns and unique annotids
+#'
+#' In case it was not read with `read_annotid_disambiguation`
+#' @noRd
+check_annotid_disambiguation <- function(annotid_disambiguation) {
   assertthat::assert_that(identical(
     sort(colnames(annotid_disambiguation)),
     c("annotid", "disambiguate", "object")
   ))
 
+  annotid_disambiguation %>%
+    dplyr::count(annotid, name = 'count') %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(count > 1) %>%
+    nrow %>%
+    assertthat::are_equal(0)
+}
+
+
+#' Checks if annot_disambiguation is up-to-date
+#'
+#' 1. Are all (annotid, object) still present in all_basiclevel_na?
+#' 2. Are all tokens that need to be disambiguated - are?
+#'
+#' Returns a list with three keys:
+#'
+#' - n_need_disambiguation - number of non-disambiguated tokens,
+#' - n_non_matched - number of (annotid, object) that no longer exist
+#' - annotid_disambiguation:
+#'   - NULL if both of the above numbers are zero,
+#'   - annotid_disambiguation with n_non_matched rows removed and
+#'     n_need_disambiguation rows added with a placeholder (constant FIXME)
+#'     in the "disambiguate" column.
+#'
+#' @noRd
+update_annotid_disambiguation <- function(all_basiclevel_na,
+                                         object_dict,
+                                         annotid_disambiguation) {
+  # Remove annotids that no longer match any tokens
+  annotid_disambiguation_matched <- annotid_disambiguation %>%
+    dplyr::semi_join(all_basiclevel_na, by = c('annotid', 'object'))
+  n_non_matched <- annotid_disambiguation %>%
+    dplyr::anti_join(annotid_disambiguation_matched,
+                     by = colnames(annotid_disambiguation)) %>%
+    nrow
+
+  # Find tokens those that need to be disambiguated but aren't
+  # List all objects that need to be disambiguated
+  ambiguous_objects <- object_dict %>%
+    dplyr::group_by(object) %>%
+    dplyr::summarise(disambiguate_count = sum(!is.na(disambiguate))) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(disambiguate_count > 0) %>%
+    dplyr::select(object)
+  # A token needs to be disambiguated if:
+  need_disambiguation <- all_basiclevel_na %>%
+    # 1. The object is ambiguous
+    dplyr::semi_join(ambiguous_objects, by = 'object') %>%
+    # It is not already disambiguated
+    dplyr::anti_join(annotid_disambiguation_matched,
+                     by = c('annotid', 'object'))
+  n_need_disambiguation <- nrow(need_disambiguation)
+
+  # Combine
+  annotid_disambiguation_for_update <-
+    if (n_need_disambiguation > 0 | n_non_matched > 0) {
+      dplyr::bind_rows(
+        annotid_disambiguation_matched,
+        need_disambiguation %>%
+          dplyr::select(object, annotid) %>%
+          dplyr::mutate(disambiguate = FIXME))
+    }
+
+  list(
+    n_need_disambiguation = n_need_disambiguation,
+    n_non_matched = n_non_matched,
+    annotid_disambiguation = annotid_disambiguation_for_update
+  )
+}
+
+#' Checks if object_dict is up-to-date
+#'
+#' - Are all objects in object_dict still present in all_basiclevel_na?
+#' - Vice versa.
+#'
+#' Returns a list with three keys:
+#' - n_new_objects - number of objects that need to be mapped to global_bl,
+#' - n_objects_to_delete - number of objects that need to be deleted,
+#' - object_dict:
+#'   - NULL, if both of the above numbers are zero,
+#'   - object_dict with n_new_objects rows added and n_deleted_objects rows
+#'     removed.
+#'
+#' @noRd
+update_object_dict <- function(all_basiclevel_na,
+                              object_dict) {
+  # Objects in all_basiclevel_na but not in object_dict
+  new_objects <- all_basiclevel_na %>%
+    dplyr::select(object) %>%
+    dplyr::anti_join(object_dict, by = 'object') %>%
+    dplyr::distinct(object)
+  n_new_objects <- nrow(new_objects)
+
+  # Objects in object_dict but not in all_basiclevel_na
+  objects_to_delete <- object_dict %>%
+    dplyr::select(object) %>%
+    dplyr::anti_join(all_basiclevel_na, by = 'object') %>%
+    dplyr::distinct(object)
+  n_objects_to_delete <- nrow(objects_to_delete)
+
+  # Combine
+  object_dict_for_update <-
+    if (n_new_objects > 0 | n_objects_to_delete > 0) {
+      dplyr::bind_rows(
+        object_dict,
+        new_objects %>%
+          mutate(disambiguate = FIXME,
+                 global_bl = FIXME))
+    }
+
+  list(n_new_objects = n_new_objects,
+       n_objects_to_delete = n_objects_to_delete,
+       object_dict = object_dict_for_update)
+}
+
+
+#' Check both mappings for up-to-dateness
+#'
+#' If either of them have to be updated:
+#' - put the files that need to be manually updated into a temporary folder,
+#' - return a message with the path to the folder and the instructions.
+#'
+#' If the mappings are up-to-date, returns NULL
+#' @noRd
+update_mappings <- function(all_basiclevel_na,
+                            object_dict,
+                            annotid_disambiguation){
+  object_dict_update <- update_object_dict(
+    all_basiclevel_na = all_basiclevel_na,
+    object_dict = object_dict)
+  annotid_disambiguation_update <-  update_annotid_disambiguation(
+    all_basiclevel_na = all_basiclevel_na,
+    object_dict = object_dict,
+    annotid_disambiguation = annotid_disambiguation)
+
+  # If there is nothing to update, return NULL
+  object_dict_ok <- is.null(object_dict_update$object_dict)
+  annotid_disambiguation_ok <- is.null(
+    annotid_disambiguation_update$annotid_disambiguation)
+  if (object_dict_ok & annotid_disambiguation_ok) {
+    return(NULL)
+  }
+
+  # We'll put the files to be updated into a temporary folder and output
+  # instructions as needed
+  temp_dir <- tempfile('global_bl_mappings_update')
+  dir.create(temp_dir, showWarnings = FALSE)
+  instructions <- glue::glue(
+    "See files inside {temp_dir}:\n")
+
+
+  # For each mapping, save the tibble
+  if (!object_dict_ok) {
+    # Save file to a temporary folder
+    filename <- 'global_bl_dictionary.csv'
+    object_dict_update$object_dict %>%
+      write_csv(file.path(temp_dir, filename))
+    # Create an instruction for the update
+    instructions <- glue::glue(
+      instructions, '\n',
+      "Update {filename}. ",
+      "Look for the cells with \"{FIXME}\" in them and fill in the ",
+      "\"global_bl\" column. ",
+      'If the object is ambigious, fill in the "disambiguate" column as well,',
+      'otherwise just delete "{FIXME}" from it.\n'
+    )}
+
+  if (!annotid_disambiguation_ok) {
+    # Save file to a temporary folder
+    filename <- 'disambiguated_rows.csv'
+    object_dict_update$object_dict %>%
+      write_csv(file.path(temp_dir, 'disambiguated_rows.csv'))
+    # Create an instruction for the update
+    instructions <- glue::glue(
+      instructions, '\n',
+      "Update {filename}. ",
+      "Look for the cells with \"{FIXME}\" in them and fill in the ",
+      "\"global_bl\" column. ",
+      'If the object is ambigious, fill in the "disambiguate" column as well,',
+      'otherwise just delete "{FIXME}" from it.\n'
+    )}
+
+  instructions <- glue::glue(
+    instructions, '\n',
+    'Once done, update the correspoding files in the global_basic_level ',
+    'repostitory.\n')
+
+  instructions
+}
+
+#' Map every row in all_basiclevel_na to its global basic level
+#'
+#' - disambiguate what we can, based on annotid_disambiguation and several
+#'   (object, basic_level) combinations,
+#' - map (object, disambiguate) to global_bl using object_dict
+#'
+#' It is assumed that the all_basiclevel_na and global_bl mappings have already
+#' been checked and so there is enough information to map every token to a
+#' global basic level. No checks are run.
+#'
+#' @noRd
+assign_global_basic_level <- function(all_basiclevel_na,
+                                      object_dict,
+                                      annotid_disambiguation) {
   # Disambiguate first based on annotid and then based on
-  with_global_bl <- all_basiclevel_na %>%
+  assignment <- all_basiclevel_na %>%
     dplyr::select(annotid, object, basic_level) %>%
     dplyr::left_join(annotid_disambiguation, by = c("annotid", "object")) %>%
     dplyr::mutate(disambiguate = dplyr::case_when(
@@ -107,15 +279,79 @@ make_global_basic_level <- function(all_basiclevel_na,
       TRUE ~ disambiguate))
 
   # Match to the global basic value
-  with_global_bl <- with_global_bl %>%
-    dplyr::left_join(object_dict, by = c("object", "disambiguate"))
+  assignment %>%
+    dplyr::left_join(object_dict, by = c("object", "disambiguate")) %>%
+    select(-disambiguate)
+}
+
+
+#' Create all_basiclevel_na with added global_bl column
+#'
+#' @param all_basiclevel_version Version tag of the `all_basiclevel` repository.
+#'   Use for testing only.
+#' @param global_bl_mappings_version Version tag of the repository with the
+#'   mappings used to match tokens with their global basic levels. Use for
+#'   testing only.
+#'
+#' Loads the latest versions of:
+#' - all_basiclevel_na from the "all_basiclevel" repository,
+#' - object_dict and annotid_disambiguation from the "global_basic_level"
+#'   repository
+#'
+#' And then tries to add a global_bl column to all_basiclevel_na if it can.
+#' If it can, it will return all_basiclevel_na with an added a column.
+#'
+#' If it can't, it will throw an error and provide instructions how to update
+#' the global basic level mappings.
+make_new_global_basic_level <- function(
+  all_basiclevel_version = NULL,
+  global_bl_mappings_version = NULL
+) {
+  # Load the data and the mappings
+  suppressWarnings({
+    all_basiclevel_na <- get_all_basiclevel(drop_basic_level_na = FALSE,
+                                            version = all_basiclevel_version)
+    assertthat::assert_that(sum(is.na(all_basiclevel_na$basic_level)) > 0)
+    global_bl_mappings <- get_global_bl_mappings(
+      version = global_bl_mappings_version)
+    object_dict <- global_bl_mappings$object_dict
+    annotid_disambiguation <- global_bl_mappings$annotid_disambiguation
+  })
+
+  # Run a few basic checks on the mappings
+  check_object_dict(object_dict)
+  check_annotid_disambiguation(annotid_disambiguation)
+
+  # Check that the mapping are complete and up-to-date with respect to what is
+  # in all_basiclevel_na.
+  update_instructions <-
+    update_mappings(
+      all_basiclevel_na = all_basiclevel_na,
+      object_dict = object_dict,
+      annotid_disambiguation = annotid_disambiguation
+    )
+  if (!is.null(update_instructions)) {
+    stop(update_instructions)
+  }
+
+  # Assign global basic level
+  assignment <- assign_global_basic_level(all_basiclevel_na,
+                                          object_dict,
+                                          annotid_disambiguation)
+  assertthat::assert_that(identical(
+    colnames(assignment),
+    c("annotid", "object", "basic_level", "global_bl")))
+
+  # Add "global_bl" column
+  with_global_bl <- all_basiclevel_na %>%
+    dplyr::inner_join(assigned %>% select(-disambiguate),
+                      by = c("annotid", "object", "basic_level"))
+
   # Check that we didn't lose any tokens, didn't create any duplicates,
   assertthat::are_equal(all_basiclevel_na$annotid, with_global_bl$annotid)
 
-  # and found global_basic_level for all tokens
-  # without_global_bl_count <- sum(is.na(with_global_bl$global_bl))
-  # warning(glue::glue('There were {without_global_bl_count} tokens for which we
-  #                     couldn\'t assign global basic level.'))
+  # Check if all the tokens have global basic level now.
+  assertthat::assert_that(sum(is.na(with_global_bl$global_bl)) == 0)
 
-  return(list(with_global_bl = with_global_bl))
+  with_global_bl
 }
--- tests/testthat/test-global_basic_level.r
+++ tests/testthat/test-global_basic_level.r
@@ -1,19 +1,28 @@
 library(digest)
 
 test_that("add_global_basic_level works as expected", {
-  all_basiclevel_na <- get_all_basiclevel(version = '0.1.3',
-                                          drop_basic_level_na = FALSE)
-  mappings_dir <- file.path(blab_data, 'global_basic_level', 'data')
-  object_dict <- read_object_dict(
-    file.path(mappings_dir, "global_bl_dictionary.csv"))
-  annotid_disambiguation <- read_annotid_disambiguation(
-    file.path(mappings_dir, "disambiguated_rows.csv"))
+  # Load the data
+  suppressWarnings({
+    all_basiclevel_na <- get_all_basiclevel(version = '0.1.3',
+                                            drop_basic_level_na = FALSE)
+    mappings <- get_global_bl_mappings(version = '0.1.0')
+    object_dict <- mappings$object_dict
+    annotid_disambiguation <- mappings$annotid_disambiguation
+  })
 
-  with_global_basic_level <- make_global_basic_level(
+  # Check that the input data haven't changed (since the versions are set above,
+  # only changes to the data-reading function can spoil things).
+  expect_equal(digest(all_basiclevel_na), "741757773603e828afa5613344e72709")
+  expect_equal(digest(object_dict), "157b4c027cf79ee5e57837a4ae89c6dc")
+  expect_equal(digest(annotid_disambiguation),
+               "0cfa534c658a1efb0ac36589d0315453")
+
+  # Do the assignment
+  global_basic_level <- assign_global_basic_level(
     all_basiclevel_na, object_dict,
-    annotid_disambiguation)$with_global_bl
+    annotid_disambiguation)
 
-  hashes <- with_global_basic_level %>%
+  hashes <- global_basic_level %>%
     mutate(across(where(is.numeric), as.numeric),
            across(where(is.factor), as.character)) %>%
     summarise(across(everything(), digest)) %>%
@@ -23,8 +32,7 @@ test_that("add_global_basic_level works as expected", {
       annotid = "207e1b1c9755effaa844e06d6d726476",
       object = "dccfe65b3fe6407b3dbf5906308eed82",
       basic_level = "08d332c4943e65a312b7b420ad4c29bf",
-      disambiguate = "ac9b8c415095981cb048e676d4e14b4f",
-      global_bl = "8b41b0f9a64f52649167dff170d23390"
+      global_bl = "63e028754c508fea4d1c923b3ebf8b47"
     )
   expect_equal(hashes, expected_hashes)
 })
