d043d960ee2b885c8ac2dcb3127a5e22d2ea7153
Zhenya Kalenkovich <e.kalenkovich@gmail.com>
2023-09-08 15:58:11 -0400

    update theme.R
    The main thing was to remove `library(tideverse)` and instead qualify all ggplot2 function calls (e.g., `theme` -> `ggplot2::theme`).
    
    Extra:
    - Added docstrings and exported the functions themselves.
    - Formatting.
    - Change `size` to `element_width` in `element_line` calls. `size` is deprecated as of ggplot2 3.4.0.

--- R/theme.R
+++ R/theme.R
@@ -3,61 +3,102 @@
 # if(Sys.info()['sysname'][[1]] == "Windows") {loadfonts(device = "win")}
 # loadfonts(device = "pdf")
 # loadfonts(device = "postscript")
-library(tidyverse)
 # fonts()
 
+#' Theme "blab" for ggplot2
+#'
+#' @return a ggplot2::theme object
+#' @export
 theme_blab <- function () {
-  theme_bw(base_size=18, base_family="") %+replace%
-    theme(
-      panel.background  = element_rect(fill = "white"),
-      panel.border = element_rect(fill = NA, colour = "black"),
-      plot.background = element_rect(fill="transparent", colour=NA),
-      legend.background = element_rect(fill = "white"),
-      legend.box.background = element_rect(colour = "black"),
-      legend.key.size = unit(1.75, units = "picas"),
-      legend.text = element_text(margin = margin(l = 3)),
-      legend.key = element_rect(fill="transparent", colour=NA),
-      axis.text = element_text(colour = "black"),
-      axis.title = element_text(size = 24),
-      panel.grid.major = element_line(colour = "grey85", size = .3),
-      panel.grid.minor = element_line(colour = "grey92", size = .3),
-      strip.background = element_rect(fill = "#440154"),
-      strip.text = element_text(colour = "white", size = 18, margin = margin(t = 5, 0, b = 5, 0))
+  ggplot2::theme_bw(base_size = 18, base_family = "") %+replace%
+    ggplot2::theme(
+      panel.background  = ggplot2::element_rect(fill = "white"),
+      panel.border = ggplot2::element_rect(fill = NA, colour = "black"),
+      plot.background = ggplot2::element_rect(fill = "transparent",
+                                              colour = NA),
+      legend.background = ggplot2::element_rect(fill = "white"),
+      legend.box.background = ggplot2::element_rect(colour = "black"),
+      legend.key.size = ggplot2::unit(1.75, units = "picas"),
+      legend.text = ggplot2::element_text(margin = ggplot2::margin(l = 3)),
+      legend.key = ggplot2::element_rect(fill = "transparent",
+                                         colour = NA),
+      axis.text = ggplot2::element_text(colour = "black"),
+      axis.title = ggplot2::element_text(size = 24),
+      panel.grid.major = ggplot2::element_line(colour = "grey85",
+                                               linewidth = .3),
+      panel.grid.minor = ggplot2::element_line(colour = "grey92",
+                                               linewidth = .3),
+      strip.background = ggplot2::element_rect(fill = "#440154"),
+      strip.text = ggplot2::element_text(
+        colour = "white",
+        size = 18,
+        margin = ggplot2::margin(t = 5, 0, b = 5, 0)
+      )
     )
 }
 
+#' Theme "spooky" for ggplot2
+#'
+#' @return a ggplot2::theme object
+#' @export
 theme_spooky <- function() {
-  theme_bw(base_size=32, base_family="serif") %+replace%
-    theme(
-      panel.background = element_rect(fill = "#FFFFFF"),
-      panel.border = element_rect(fill = "transparent", colour = "grey50"),
-      legend.text = element_text(margin = margin(l = 0), colour = "white"),
-      axis.text = element_text(colour = "white"),
-      axis.title = element_text(colour = "white"),
-      plot.background = element_rect(fill = "#0f0838", colour = "#0f0838"),
-      legend.background = element_rect(fill = "#0f0838", colour = "#0f0838"),
-      legend.key = element_blank(),
-      strip.background = element_rect(fill = "#102b52", colour = "grey50"),
-      strip.text = element_text(colour = "white", size = 24, margin = margin(t = 5, 0, b = 5, 0))
+  ggplot2::theme_bw(base_size = 32, base_family = "serif") %+replace%
+    ggplot2::theme(
+      panel.background = ggplot2::element_rect(fill = "#FFFFFF"),
+      panel.border = ggplot2::element_rect(fill = "transparent",
+                                           colour = "grey50"),
+      legend.text = ggplot2::element_text(margin = ggplot2::margin(l = 0),
+                                          colour = "white"),
+      axis.text = ggplot2::element_text(colour = "white"),
+      axis.title = ggplot2::element_text(colour = "white"),
+      plot.background = ggplot2::element_rect(fill = "#0f0838",
+                                              colour = "#0f0838"),
+      legend.background = ggplot2::element_rect(fill = "#0f0838",
+                                                colour = "#0f0838"),
+      legend.key = ggplot2::element_blank(),
+      strip.background = ggplot2::element_rect(fill = "#102b52",
+                                               colour = "grey50"),
+      strip.text = ggplot2::element_text(
+        colour = "white",
+        size = 24,
+        margin = ggplot2::margin(t = 5, 0, b = 5, 0)
       )
+    )
 }
 
+#' Theme "AMERICA" for ggplot2
+#'
+#' @return a ggplot2::theme object
+#' @export
 theme_AMERICA <- function() {
-  theme_bw(base_size=18, base_family="sans") %+replace%
-    theme(
-      panel.background = element_rect(fill = "#FFFFFF"),
-      panel.border = element_rect(fill = "transparent", colour = "grey50"),
-      legend.text = element_text(margin = margin(l = 0), colour = "white"),
-      axis.text = element_text(colour = "white"),
-      axis.title = element_text(colour = "white"),
-      panel.grid.major = element_line(colour = "#BF0A30", size = 8),
-      panel.grid.minor = element_line(colour = "#BF0A30", size = 8),
-      panel.grid.major.x = element_line(colour = "transparent", size = .3),
-      panel.grid.minor.x = element_line(colour = "transparent", size = .3),
-      plot.background = element_rect(fill = "#002868", colour = "#002868"),
-      legend.background = element_rect(fill = "#002868", colour = "#002868"),
-      legend.key = element_blank(),
-      strip.background = element_rect(fill = "white", colour = "grey50"),
-      strip.text = element_text(colour = "#002868", size = 18, margin = margin(t = 5, 0, b = 5, 0))
+  ggplot2::theme_bw(base_size = 18, base_family = "sans") %+replace%
+    ggplot2::theme(
+      panel.background = ggplot2::element_rect(fill = "#FFFFFF"),
+      panel.border = ggplot2::element_rect(fill = "transparent",
+                                           colour = "grey50"),
+      legend.text = ggplot2::element_text(margin = ggplot2::margin(l = 0),
+                                          colour = "white"),
+      axis.text = ggplot2::element_text(colour = "white"),
+      axis.title = ggplot2::element_text(colour = "white"),
+      panel.grid.major = ggplot2::element_line(colour = "#BF0A30",
+                                               linewidth = 8),
+      panel.grid.minor = ggplot2::element_line(colour = "#BF0A30",
+                                               linewidth = 8),
+      panel.grid.major.x = ggplot2::element_line(colour = "transparent",
+                                                 linewidth = .3),
+      panel.grid.minor.x = ggplot2::element_line(colour = "transparent",
+                                                 linewidth = .3),
+      plot.background = ggplot2::element_rect(fill = "#002868",
+                                              colour = "#002868"),
+      legend.background = ggplot2::element_rect(fill = "#002868",
+                                                colour = "#002868"),
+      legend.key = ggplot2::element_blank(),
+      strip.background = ggplot2::element_rect(fill = "white",
+                                               colour = "grey50"),
+      strip.text = ggplot2::element_text(
+        colour = "#002868",
+        size = 18,
+        margin = ggplot2::margin(t = 5, 0, b = 5, 0)
+      )
     )
 }
