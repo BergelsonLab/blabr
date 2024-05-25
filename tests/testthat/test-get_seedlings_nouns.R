76493149795bffe4626cb9f25e88e0ba215b712e
Zhenya Kalenkovich <e.kalenkovich@gmail.com>
2024-05-25 14:46:46 -0400

    test: switch to v2.0.0-dev.3
    I've yanked v2.0.0-dev so that I didn't have to account for column types for both versions.

--- tests/testthat/test-get_seedlings_nouns.R
+++ tests/testthat/test-get_seedlings_nouns.R
@@ -7,7 +7,7 @@ test_that("the main table loads at all", {
 test_that("get_seedlings_nouns and friends work", {
   versions_to_test <- c('0.0.0.9016',  # a dev version
                         'v1.0.0',  # a public version
-                        'v2.0.0-dev')  # a version with extra columns added
+                        'v2.0.0-dev.3')  # a version with extra columns added
   for (version in versions_to_test) {
     for (table in c('seedlings-nouns', 'regions', 'recordings',
                     'sub-recordings')) {
