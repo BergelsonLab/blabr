2b19d9c112ac5cbd92124e3c35bba35a237bee50
Kien Nguyen <kientnguyen.ntk@gmail.com>
2026-04-29 15:01:31 -0400

    code for wrangling CDI and for getting VIHI CDI

--- R/cdi_wrangling.R
+++ R/cdi_wrangling.R
@@ -1,3 +1,188 @@
+#' Clean up raw CDI output (item + summary) for any project administered through 
+#' WebCDI.
+#' 
+#' @param filepath Dataframe of the csv file downloaded from WebCDI 
+#' (you should use the csv with both item level and sumary data)
+#' @param form Which kind of cdi form is this (`WG` or `WS`). 
+#' Will only include columns from each form.
+#' @param table Which subset of the output to include? 
+#' All table include id-related columns: "study_name", "subject_id", "repeat_num".
+#' - "summary" (default): each row is one cdi form, including aggregated score 
+#' for each form (e.g. total words produced, total number of first gestures, etc.).
+#' - "wordlevel": each row is an item on one cdi form, with an "item" column 
+#' for the name of the item and a "response" column.
+#' - "raw": this is the rawest form of the data as downloaded from webcdi, with 
+#' id-related columns added, each row is one cdi form, including columns for 
+#' each item and columns for aggregated score, 
+#' as well as metadata columns (e.g. date created, webcdi link, admin id, etc.). 
+#' Is not affected by `withDemographic` and `justWord` parameters.
+#' @param withDemographic Should the data include answers to demographic questions 
+#' (e.g. sibling count, birth order, caregive info, etc.)? These columns are not 
+#' treated as item, so if you select `wordlevel_long` table, the demographic 
+#' answers will be included in every row. If you select `raw` table, demographic 
+#' columns will always be included. `FALSE` is default. 
+#' @param justWord Should the data include only vocabulary checklist item? If 
+#' `TRUE` (default), returns only `Words Produced` and `Words Understood` related 
+#' columns for `summary` table and vocabulary items for `wordlevel` table. If 
+#' `FALSE`, will include gestures-related items for `WG` form and sentence-related 
+#' items for `WS` form. If you select `raw` table, all items will be included 
+#' regardless of this variable. 
+#' @param rename For `WS` form, do we standardize the name of the 
+#' word-related summary item to be the same as `WG` form. If `TRUE` (default), the 
+#' three summary columns in WS named `Total Produced`, `Total Produced Percentile-sex`, 
+#' and `Total Produced Percentile-both` will  changed to `Words Produced`, 
+#' `Words Produced Percentile-sex` and `Words Produced Percentile-both`. Otherwise, 
+#' they will remain unchanged
+#' 
+#' @return A dataframe of the wrangled CDI output according to the parameters
+#' @export
+#' 
+wrangle_web_cdi <- function(cdi_df,
+                            form = c("WG", "WS"),
+                            table = c("summary", "wordlevel", "raw"),
+                            withDemographic = FALSE,
+                            justWord = TRUE,
+                            rename = TRUE) {
+  form <- match.arg(form)
+  table <- match.arg(table)
+  
+  wg_key <- readr::read_csv(system.file("extdata", "English_WG_dictionary.csv", package = "blabr"))
+  ws_key <- readr::read_csv(system.file("extdata", "English_WS_dictionary.csv", package = "blabr"))
+  wg_cols <- wg_key$item
+  ws_cols <- ws_key$item
+  
+  item_cols <- union(wg_cols, ws_cols)
+  summary_cols <- union(wg_summary_cols, ws_summary_cols) # these are defined in in_cdi.R
+  
+  all_cols <- colnames(cdi_df)
+  id_cols <- c("study_name", "subject_id", "repeat_num")
+  admin_cols <- c("opt_out", "local_lab_id", "administration_id", "link", "completed", "completedBackgroundInfo", "due_date", "last_modified", "created_date", "completed_date", "event_id")
+  demographic_cols <- all_cols[!all_cols %in% c(id_cols, admin_cols, summary_cols, item_cols)]
+  
+  if (withDemographic) {
+    cols_to_keep <- c(id_cols, demographic_cols)
+  } else {
+    cols_to_keep <- id_cols
+  }
+  
+  if (justWord) {
+    wg_cols <- wg_key %>% 
+      dplyr::filter(item_type == "word") %>% 
+      dplyr::pull(item)
+    ws_cols <- ws_key %>% 
+      dplyr::filter(item_type == "word") %>% 
+      dplyr::pull(item)
+    wg_summary_cols <- wg_summary_cols[stringr::str_detect(wg_summary_cols, "(Produced)|(Understood)")]
+    ws_summary_cols <- ws_summary_cols[stringr::str_detect(ws_summary_cols, "(Produced)|(Understood)")]
+  }
+  
+  # decide for each form
+  if (form == "WG") {
+    item_cols <- wg_cols
+    summary_cols <- wg_summary_cols
+  } else if (form == "WS") {
+    if (rename) {
+      cdi_df <- cdi_df %>% 
+        dplyr::rename(
+          `Words Produced` = `Total Produced`, 
+          `Words Produced Percentile-sex` = `Total Produced Percentile-sex`, 
+          `Words Produced Percentile-both`= `Total Produced Percentile-both`
+        ) # so we have the same name for form WG and WS
+    }
+    item_cols <- ws_cols
+    summary_cols <- ws_summary_cols
+  } 
+  
+  if (table != "wordlevel") {
+    if (table == "summary") {
+      cols_to_keep <- c(cols_to_keep, summary_cols)
+    } else {
+      cols_to_keep <- all_cols
+    }
+    cdi_df <- cdi_df %>% 
+      dplyr::select(all_of(cols_to_keep))
+    
+  } else {
+    cdi_df <- cdi_df %>% 
+      dplyr::select(c(cols_to_keep, item_cols)) %>% 
+      tidyr::pivot_longer(cols = all_of(item_cols), names_to = "item", values_to = "response")
+  }
+  
+  return(cdi_df)
+}
+
+#' Load VIHI cdi output from BLAB_SHARE (server will need to be mounted)
+#' @inheritParams wrangle_web_cdi
+#' @param population Which group of participants to include (`VI`, `HI`, or 
+#' `VIHI` (which will include both))?
+#' @param form Which kind of cdi forms to include (`WG`, `WS`, or `both`)? 
+#' The data will only include items that are featured on said form.
+#' 
+#' @export
+#' 
+get_vihi_cdi <- function(population = c("VIHI", "VI", "HI", "TD"),
+                         form = c("all", "WG", "WS"),
+                         table = c("summary", "wordlevel", "raw"),
+                         withDemographic = FALSE,
+                         justWord = TRUE) {
+  population <- match.arg(population)
+  form <- match.arg(form)
+  table <- match.arg(table)
+  
+  cdi_path <- file.path(get_blab_share_path(), "VIHI/Surveys/CDI/VIHI/VIHI_CDI_wordlevel.csv")
+  cdi_full <- readr::read_csv(cdi_path) %>% 
+    dplyr::mutate(Population = stringr::str_sub(VIHI_ID, 1, 2)) 
+  
+  vihi_id_key <- cdi_full %>%
+    dplyr::select(VIHI_ID, Population, Form, exact_age, subject_id, repeat_num, study_name)
+
+  cdi_wg <- cdi_full %>%
+    dplyr::filter(Form == "WG") %>%
+    dplyr::select(-VIHI_ID, -Population, -Form, -exact_age)
+
+  cdi_ws <- cdi_full %>%
+    dplyr::filter(Form == "WS") %>%
+    dplyr::select(-VIHI_ID, -Population, -Form, -exact_age)
+
+  cdi_wg_wrangled <- wrangle_web_cdi(cdi_wg,
+                                     form = "WG",
+                                     table = table,
+                                     withDemographic = withDemographic,
+                                     justWord = justWord,
+                                     rename = FALSE) %>%
+    dplyr::mutate(Form = "WG") %>%
+    dplyr::left_join(vihi_id_key)
+
+
+  cdi_ws_wrangled <- wrangle_web_cdi(cdi_ws,
+                                     form = "WS",
+                                     table = table,
+                                     withDemographic = withDemographic,
+                                     justWord = justWord,
+                                     rename = FALSE) %>%
+    dplyr::mutate(Form = "WS") %>%
+    dplyr::left_join(vihi_id_key)
+
+  if (form == "all") {
+    final_cdi <- dplyr::bind_rows(cdi_wg_wrangled, cdi_ws_wrangled)
+  } else if (form == "WS") {
+    final_cdi <- cdi_ws_wrangled
+  } else {
+    final_cdi <- cdi_wg_wrangled
+  }
+
+  if (population != "VIHI") {
+    final_cdi <- final_cdi %>%
+      dplyr::filter(Population == population)
+  }
+
+  final_cdi <- final_cdi %>%
+    dplyr::select(-study_name) %>%
+    dplyr::select(VIHI_ID, Population, Form, exact_age, everything())
+
+  return(final_cdi)
+}
+
 #' Select all word item columns for a cdi spreadsheet
 #'
 #' @param data a dataframe of the original cdi csv
