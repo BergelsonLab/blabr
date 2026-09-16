a08c0b755df60378e95743373437d01544f84b8b
Kien Nguyen <kientnguyen.ntk@gmail.com>
2026-09-16 15:34:23 -0400

    exclusion warning and code

--- R/blab_participants.R
+++ R/blab_participants.R
@@ -54,9 +54,24 @@ wrangle_blab_participants <- function(df) {
 #' @export
 get_blab_participants <- function() {
   participants_path <- file.path(get_blab_share_path(), "experimental_projects/participants.xlsx")
+  message("Reading all blab_wide participants data...")
+  message("Certain participants have been excluded due to not finishing a study or being rescheduled and assigned a new ID, which might results in gaps in the local subject ID. Check the exclusion list for which participants are excluded, and then check each project's tracking sheet for more details.")
   
   participants <- readxl::read_xlsx(participants_path, sheet = "Participants") %>%
     # Any prelim cleaning, selecting columns, etc.
     wrangle_blab_participants()
   return(participants)
+}
+
+#' Get the list of excluded participants from the blab_wide tracking sheet
+#' 
+#' @param 
+#' @param 
+#' 
+#' @export
+get_blab_excluded_participants <- function() {
+  participants_path <- file.path(get_blab_share_path(), "experimental_projects/participants.xlsx")
+  
+  exclusion <- readxl::read_xlsx(participants_path, sheet = "Excluded") 
+  return(exclusion)
 }
